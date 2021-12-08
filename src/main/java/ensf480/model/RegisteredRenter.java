d91f981119b4dbbff644bb3f80b553363b50d8e9
TrueThaumaturge <quentin.jennings@ucalgary.ca>
2021-12-07 18:12:27 -0700

    Working on RegisteredRenter methods

--- src/main/java/ensf480/model/RegisteredRenter.java
+++ src/main/java/ensf480/model/RegisteredRenter.java
@@ -6,11 +6,30 @@ import java.util.ArrayList;
 public class RegisteredRenter extends Account implements ListingObserver {
 
     private SearchCriteria notifCriteria; //the user's defined criteria to get notified by
-    private boolean isSubscribed = false;
     private ArrayList<Email> inbox = new ArrayList<>();
 
     public RegisteredRenter(String username, String password, String fname, String lname) {
         super(username, password, fname, lname);
+    }
+
+    public void addEmail(Email email) {
+        inbox.add(email);
+    }
+
+    public void removeEmail(int index) {
+        inbox.remove(index);
+    }
+
+    public void clearInbox() {
+        inbox.clear();
+    }
+
+    public void subscribe(SearchCriteria notifCriteria) {
+        this.notifCriteria = notifCriteria;
+    }
+
+    public void unsubscribe() {
+        notifCriteria = null;
     }
 
     public void update() {
